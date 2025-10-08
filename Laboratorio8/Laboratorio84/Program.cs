c9472b4dfea2db9cf114eabf1522e0406f489598
Jose Luna <joselunabbh1@gmail.com>
2025-10-08 00:07:52 -0500

    subir lab 8

---
+++ Laboratorio8/Laboratorio84/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Laboratorio84
+{
+    // Primera parte de la clase Coordenadas (contiene campos y constructor)
+    public partial class Coordenadas
+    {
+        private int x;
+        private int y;
+
+        public Coordenadas(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    // Segunda parte de la clase Coordenadas (contiene el método)
+    public partial class Coordenadas
+    {
+        public void VerCoordenadas()
+        {
+            Console.WriteLine("Coordenadas: {0}, {1}", x, y);
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // El objeto usa miembros definidos en ambas partes parciales
+            Coordenadas misCoords = new Coordenadas(10, 15);
+            misCoords.VerCoordenadas();
+        }
+    }
+}
