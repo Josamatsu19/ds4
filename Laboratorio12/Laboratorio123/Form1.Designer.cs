1da843f9ab6f7832657f8782622a367dbf39f8f7
Josamatsu19 <joselunabbh1@gmail.com>
2025-10-20 23:50:07 -0500

    subir parte 3 del lab 12

---
+++ Laboratorio12/Laboratorio123/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Laboratorio123
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblC = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lblS = new System.Windows.Forms.Label();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.txtS = new System.Windows.Forms.TextBox();
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(122, 35);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(53, 16);
+            this.lblA.TabIndex = 0;
+            this.lblA.Text = "Lado A:";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(122, 67);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(53, 16);
+            this.lblB.TabIndex = 1;
+            this.lblB.Text = "Lado B:";
+            // 
+            // lblC
+            // 
+            this.lblC.AutoSize = true;
+            this.lblC.Location = new System.Drawing.Point(122, 97);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(53, 16);
+            this.lblC.TabIndex = 2;
+            this.lblC.Text = "Lado C:";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(298, 35);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 22);
+            this.txtA.TabIndex = 3;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(298, 67);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 22);
+            this.txtB.TabIndex = 4;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(298, 97);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(100, 22);
+            this.txtC.TabIndex = 5;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(137, 141);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(219, 140);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Limpiar";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(301, 140);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 8;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // lblS
+            // 
+            this.lblS.AutoSize = true;
+            this.lblS.Location = new System.Drawing.Point(125, 191);
+            this.lblS.Name = "lblS";
+            this.lblS.Size = new System.Drawing.Size(91, 16);
+            this.lblS.TabIndex = 10;
+            this.lblS.Text = "Seriperìmetro:";
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Location = new System.Drawing.Point(125, 226);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(39, 16);
+            this.lblArea.TabIndex = 11;
+            this.lblArea.Text = "Area:";
+            // 
+            // txtS
+            // 
+            this.txtS.Location = new System.Drawing.Point(298, 191);
+            this.txtS.Name = "txtS";
+            this.txtS.Size = new System.Drawing.Size(100, 22);
+            this.txtS.TabIndex = 12;
+            // 
+            // txtArea
+            // 
+            this.txtArea.Location = new System.Drawing.Point(298, 226);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.Size = new System.Drawing.Size(100, 22);
+            this.txtArea.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtArea);
+            this.Controls.Add(this.txtS);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.lblS);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.lblC);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label lblS;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.TextBox txtS;
+        private System.Windows.Forms.TextBox txtArea;
+    }
+}
+
