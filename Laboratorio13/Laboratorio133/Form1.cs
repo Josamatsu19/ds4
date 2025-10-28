68b97f904b79545c5d103a3d2fa5906100a193de
Josamatsu19 <joselunabbh1@gmail.com>
2025-10-28 00:50:44 -0500

    subir lab 13

---
+++ Laboratorio13/Laboratorio133/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorio133 
+{
+    public partial class Form1 : Form
+    {
+        string connectionString =
+            @"Server=JOSELUNA;Database=Northwind;TrustServerCertificate=true;Integrated Security=SSPI;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection conexion = new SqlConnection(connectionString);
+                 conexion.Open();
+                 MessageBox.Show("Se abrió la conexión con el servidor SQL Server y se seleccionó la base de datos");
+                
+                ListarProductos();
+
+                 conexion.Close(); 
+                 MessageBox.Show("Se cerró la conexión.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error de Conexión: " + ex.Message);
+            }
+        }
+
+        private void ListarProductos()
+        {
+            string query = "SELECT ProductName FROM [dbo].[Products]";
+
+            using (SqlConnection conexion = new SqlConnection(connectionString))
+            using (SqlCommand comando = new SqlCommand(query, conexion))
+            {
+                try
+                {
+                    conexion.Open();
+
+                    using (SqlDataReader reader = comando.ExecuteReader())
+                    {
+                        listBox1.Items.Clear();
+
+                        while (reader.Read())
+                        {
+                            listBox1.Items.Add(reader["ProductName"].ToString());
+                        }
+
+                        MessageBox.Show("Productos listados con éxito.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al ejecutar la consulta: " + ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Parcial1/Parcial12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Parcial12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+            int resu = 0;
+
+            Console.Write("Introduce número de términos: ");
+            if (!int.TryParse(Console.ReadLine(), out int n) || n <= 0)
+            {
+                Console.WriteLine("Entrada inválida. Debe ser un número mayor que 0.");
+                return;
+            }
+            for (int i = 0; i <= n; i++)
+            {
+                resu = a + b;
+                a = b;
+                b = resu;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Resultado: " + a);
+        }
+    }
+}
